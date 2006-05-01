d8b08a908c5dba5b08fcad030ac86cf4ed31dd5b
paradoxon <paradoxon@f08798fa-4112-0410-b753-e4f865caae7a>
2006-05-01 15:22:11 +0000

    Moved PDocloader stuff to Tools because it belongs more to Tools than to Documentmanagment changed the makefile to build after this change
    git-svn-id: http://svn.osdrawer.net/projectconcept@7 f08798fa-4112-0410-b753-e4f865caae7a

--- trunk/src/Document/PDocument.h
+++ trunk/src/Document/PDocument.h
@@ -73,119 +73,119 @@ public:
 	/**
 	 * Bounds returns the Bounds of the Document wicht enclose everey Node in the Document
 	 */	
-	virtual	BRect				Bounds(void){return bounds;};
+			BRect				Bounds(void){return bounds;};
 	/**
 	 * Resizes the Document (mostly called from Commands)
 	 */	
-	virtual void				Resize(float toX,float toY);
+			void				Resize(float toX,float toY);
 
 	/**
 	 * Returns if the Document is dirty .. not implemented at the moment
 	 */	
-	virtual	bool				IsDirty(void){return dirty;};
+			bool				IsDirty(void){return dirty;};
 	/**
 	 * IsModified returns if there was any change in the document
 	 */	
-	virtual	bool				IsModified(void){return modified;}
+			bool				IsModified(void){return modified;}
 	/**
 	 * SetModified set the Document to a modified state.. 
 	 * if the user now try to quit he is asked
 	 */	
-	virtual void				SetModified(void){modified=true;};
+			void				SetModified(void){modified=true;};
 	/**
 	 * ResetModified set the Document to a unmodified state.. 
 	 */	
-	virtual	void				ResetModified(void){modified=false;};
+			void				ResetModified(void){modified=false;};
 
-	virtual BList				*GetChangedNodes(void){return valueChanged;};
+			BList				*GetChangedNodes(void){return valueChanged;};
 
-	virtual BList				*GetSelected(void){return selected;};
-	virtual	BList				*GetAllNodes(void){return allNodes;};
-	virtual BList				*GetAllConnections(void){return allConnections;};
-	virtual BList				*GetTrash(void){return trashed;};
+			BList				*GetSelected(void){return selected;};
+			BList				*GetAllNodes(void){return allNodes;};
+			BList				*GetAllConnections(void){return allConnections;};
+			BList				*GetTrash(void){return trashed;};
 
 	//* Returns the DocumentSettings in a BMessage
-	virtual	BMessage*			DocumentSettings(void){return documentSetting;};
-	virtual	void				SetDocumentSettings(BMessage *message);
+			BMessage*			DocumentSettings(void){return documentSetting;};
+			void				SetDocumentSettings(BMessage *message);
 
 
 	//* Returns the PrintSettings as a BMessage if there is no PrintSettings a dialog shows up where the printsettings shoud be configured :-)
-	virtual	BMessage*			PrintSettings(void);
+			BMessage*			PrintSettings(void);
 	/**Set the PrintSettings 
 	 * @warning There is no check if the printersettings are valid!!!!!
 	 */
-	virtual	void				SetPrintSettings(BMessage *settings);
+			void				SetPrintSettings(BMessage *settings);
 	/**Print´s the lastly activated Editor
 	 *@todo some error handling and printerSettings.. autocall
 	 */
-	virtual void				Print(void);
+			void				Print(void);
 
 	/**Set the save and load entry
 	 */
-	virtual	void				SetEntry(entry_ref *saveEntry,const char *name);
+			void				SetEntry(entry_ref *saveEntry,const char *name);
 	/**Set the save and load entry*/
-	virtual	void				SetEntry(entry_ref *newEntry){entryRef = new entry_ref(*newEntry);};
+			void				SetEntry(entry_ref *newEntry){entryRef = new entry_ref(*newEntry);};
 	/**return the save and load entry*/
-	virtual entry_ref*			Entry(void){return entryRef;};
+			entry_ref*			Entry(void){return entryRef;};
 
 	/**save the file to the entry
 	 *@see SetEntry
 	 */
-	virtual	void				Save(void);
+			void				Save(void);
 	/**save the file from the entry
 	 *
 	 *@todo mime check
 	 *
 	 *@see SetEntry
 	 */
-	virtual	void				Load(void);
+			void				Load(void);
 
 	/**shows a savePanel.. wich pass back an entry and autmatically call SetEntry
 	 *@see SetEntry
 	 */
-	virtual	void				SavePanel();
+			void				SavePanel();
 
-	virtual	bool				QuitRequested(void);
+			bool				QuitRequested(void);
 
-	virtual	WindowManager*		GetWindowManager(void){return windowManager;};
-	virtual	PCommandManager*	GetCommandManager(void){return commandManager;};
-	virtual	PEditorManager*		GetEditorManager(void){return editorManager;};
+			WindowManager*		GetWindowManager(void){return windowManager;};
+			PCommandManager*	GetCommandManager(void){return commandManager;};
+			PEditorManager*		GetEditorManager(void){return editorManager;};
 
 	
-	virtual	PDocumentManager	*BelongTo(void){return documentManager;};
+			PDocumentManager	*BelongTo(void){return documentManager;};
 
 
 	//++++++++++++++++++++++PMenuAccess
 	
-	virtual	BMenu*				GetMenu(const char* signatur);
-	virtual	BMenuItem*			GetMenuItem(const char* signatur);
+			BMenu*				GetMenu(const char* signatur);
+			BMenuItem*			GetMenuItem(const char* signatur);
 
-	virtual	ToolBar*			GetToolBar(const char *signatur);
-	virtual	ToolMenu*			GetToolMenu(const char* toolbarSignature,const char *signature);
-	virtual	ToolItem*			GetToolItem(const char* toolbarSignature,const char *signature);
+			ToolBar*			GetToolBar(const char *signatur);
+			ToolMenu*			GetToolMenu(const char* toolbarSignature,const char *signature);
+			ToolItem*			GetToolItem(const char* toolbarSignature,const char *signature);
 
-	virtual	status_t			AddMenu(BMenu *menu,int32 index = -1);
-	virtual	status_t			AddMenuItem(const char *menuSignatur, BMenuItem *menuItem,int32 index = -1);
-	virtual	status_t			AddToolBar(ToolBar *toolbar);
-	virtual	status_t			AddToolMenu(const char *toolbarSignatur,ToolMenu *toolMenu);
-	virtual	status_t			AddToolItem(const char *toolbarSignatur,const char *toolmenuSignatur,ToolItem *toolItem,int32 index = -1);
+			status_t			AddMenu(BMenu *menu,int32 index = -1);
+			status_t			AddMenuItem(const char *menuSignatur, BMenuItem *menuItem,int32 index = -1);
+			status_t			AddToolBar(ToolBar *toolbar);
+			status_t			AddToolMenu(const char *toolbarSignatur,ToolMenu *toolMenu);
+			status_t			AddToolItem(const char *toolbarSignatur,const char *toolmenuSignatur,ToolItem *toolItem,int32 index = -1);
 
 
-	virtual status_t			RemoveMenu(const char* menuSignature) ;
-	virtual status_t			RemoveMenuItem(const char* menuItemSignature);
+			status_t			RemoveMenu(const char* menuSignature) ;
+			status_t			RemoveMenuItem(const char* menuItemSignature);
 
-	virtual status_t			RemoveToolBar(const char* signature);
-	virtual status_t			RemoveToolMenu(const char* toolbarSignature,const char* toolmenuSignature) ;
-	virtual status_t			RemoveToolItem(const char* toolbarSignature,const char* toolitemSignature);
+			status_t			RemoveToolBar(const char* signature);
+			status_t			RemoveToolMenu(const char* toolbarSignature,const char* toolmenuSignature) ;
+			status_t			RemoveToolItem(const char* toolbarSignature,const char* toolitemSignature);
 	
-	virtual BRect*				GetPrintRect(void){return printableRect;};
+			BRect*				GetPrintRect(void){return printableRect;};
 
-	virtual	BMessage*			FindObject(BPoint *where);
+			BMessage*			FindObject(BPoint *where);
 
 protected:
-	virtual	void				Init(void);
-	virtual	void				Init(BMessage *archive);
-	virtual	BMessage			*ReIndex(BList *nodeList);
+			void				Init(void);
+			void				Init(BMessage *archive);
+			BMessage			*ReIndex(BList *nodeList);
 
 			
 			
