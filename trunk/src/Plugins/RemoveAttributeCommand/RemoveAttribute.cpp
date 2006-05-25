60b004f1bc49a6efb685ad19c3e3d2170c5be3f0
paradoxon <paradoxon@f08798fa-4112-0410-b753-e4f865caae7a>
2006-05-25 11:19:58 +0000

    Added a base for RemoveAttributeCommand to the Project
    git-svn-id: http://svn.osdrawer.net/projectconcept@76 f08798fa-4112-0410-b753-e4f865caae7a

---
+++ trunk/src/Plugins/RemoveAttributeCommand/RemoveAttribute.cpp
@@ -0,0 +1,181 @@
+#include "ProjectConceptorDefs.h"
+#include "RemoveAttribute.h"
+
+
+RemoveAttribute::RemoveAttribute():PCommand()
+{
+}
+
+void RemoveAttribute::Undo(PDocument *doc,BMessage *undo)
+{
+	BMessage	*undoMessage	= new BMessage();
+	BMessage	*subGroup		= new BMessage();
+	BMessage	*selectNodes	= new BMessage();
+	BList		*changed		= doc->GetChangedNodes();
+	BMessage	*node			= NULL;
+	char		*name			= NULL;
+	char		*compareName	= NULL;
+	char		*subGroupName	= NULL;
+	bool		selected		= false;
+	int32		index			= 0;
+	int32		count			= 0;
+	type_code	type			= B_ANY_TYPE;
+	type_code	typeFound		= NULL;
+	int32		lastIndex		= -1;
+	int32		i				= 0;
+	status_t	err				= B_OK;
+	void		*dummyData		= NULL;
+	ssize_t		oldSize			= 0;
+	PCommand::Undo(doc,undoMessage);
+	undo->FindMessage("RemoveAttribute::Undo" ,undoMessage);
+	while (undo->FindPointer("node",i,(void **)&node) == B_OK)
+	{
+		err = B_OK;
+		err = undo->FindString("name",i,(const char**)&name);
+		err = err | undo->FindInt32("type",i,(int32 *)&type);
+		lastIndex = -1;
+		if ( (undo->FindString("subgroup",i,(const char**)&subGroupName) == B_OK) && (node->FindMessage(subGroupName,subGroup) == B_OK) )
+		{
+			index = 0;
+          			while (subGroup->GetInfo(B_ANY_TYPE, index, (const char **)&compareName, &typeFound, &count) == B_OK)
+    		{
+    			if ( (strcmp(name,compareName)) && (type == typeFound) )
+    				lastIndex = count;
+       		}  
+			subGroup->RemoveData(name,lastIndex);
+			node->ReplaceMessage(subGroupName,subGroup);
+		}
+		else
+		{
+			index = 0;
+			while (node->GetInfo(B_ANY_TYPE, index,(const char**) &compareName, &typeFound, &count) == B_OK)
+    		{
+    			if ( (strcmp(name,compareName)) && (type == typeFound) )
+    				lastIndex = count;
+       		}  
+			node->RemoveData(name,lastIndex);
+		}
+		i++;
+		changed->AddItem(node);
+	}
+	if ( (undo->FindBool("selected",&selected) == B_OK) && (selected==true) )
+	{
+		undoMessage->FindMessage("selectedNodes",selectNodes);
+		err 		= B_OK;
+		int32 	i	= 0;
+		err = undo->FindString("name",i,(const char**)&name);
+		err = err | undo->FindInt32("type",i,(int32 *)&type);
+		if ( (undo->FindString("subgroup",i,(const char**)&subGroupName) != B_OK))
+			subGroupName=NULL;	
+		while (selectNodes->FindPointer("node",i,(void **)&node) == B_OK)
+		{
+			lastIndex = -1;
+			changed->AddItem(node);
+			if (subGroupName)
+			{
+				index = 0;
+				while (subGroup->GetInfo(B_ANY_TYPE, index,(const char**) &compareName, &typeFound, &count) == B_OK)
+    			{
+    				if ( (strcmp(name,compareName)) && (type == typeFound) )
+    					lastIndex = count;
+       			}  
+				subGroup->RemoveData(name,lastIndex);
+				node->ReplaceMessage(subGroupName,subGroup);
+			}
+			else
+			{
+				index = 0;
+				while (node->GetInfo(B_ANY_TYPE, index,(const char**) &compareName, &typeFound, &count) == B_OK)
+    			{
+    				if ( (strcmp(name,compareName)) && (type == typeFound) )
+	    				lastIndex = count;
+    	   		}
+    			node->RemoveData(name,lastIndex);
+	   	   	}
+			i++;
+		}
+	}
+	doc->SetModified();
+}
+
+BMessage* RemoveAttribute::Do(PDocument *doc, BMessage *settings)
+{
+	BList		*changed		= doc->GetChangedNodes();
+	BList		*selection		= doc->GetSelected();
+	BMessage	*undoMessage	= new BMessage();
+	BMessage	*node			= NULL;
+	BMessage	*subGroup		= new BMessage();
+	BMessage	*selectedNodes	= new BMessage();
+	int32		i				= 0;
+	bool		selected		= false;
+	char		*name			= NULL;
+	char		*subGroupName	= NULL;
+	type_code	type			= B_ANY_TYPE;
+	void*		newValue		= NULL;
+	void*		oldValue		= NULL;
+	ssize_t		size			= 0;
+	ssize_t		oldSize			= 0;
+	status_t	err				= B_OK;
+	while (settings->FindPointer("node",i,(void **)&node) == B_OK)
+	{
+		err = settings->FindString("name",i,(const char**)&name);
+		err = err | settings->FindInt32("type",i,(int32 *)&type);
+		err = settings->FindData("newAttribute", type, i, (const void **)&newValue, &size);
+		if ( (settings->FindString("subgroup",i,(const char**)&subGroupName) == B_OK))
+		{
+			err = node->FindMessage(subGroupName,subGroup);
+			if (err == B_OK) 
+				subGroup->AddData(name,type,newValue,size);
+			node->ReplaceMessage(subGroupName,subGroup);
+		}
+		else
+		{
+			if (err == B_OK) 
+				node->AddData(name,type,newValue,size);
+		}
+		changed->AddItem(node);
+		i++;
+	}
+	if ( (settings->FindBool("selected",&selected) == B_OK) && (selected==true) )
+	{
+		err = settings->FindString("name",i,(const char**)&name);
+		err = err | settings->FindInt32("type",i,(int32 *)&type);
+		err = err | settings->FindData("newAttribute",type,i,(const void **)&newValue,&size);
+		if ( (settings->FindString("subgroup",i,(const char**)&subGroupName) != B_OK))
+		subGroupName=NULL;
+		for (int32 i=0;i<selection->CountItems();i++)
+		{
+			node =(BMessage *) selection->ItemAt(i);
+			selectedNodes->AddPointer("node",node);
+			changed->AddItem(node);
+			if (subGroupName)
+			{
+				err = node->FindMessage(subGroupName,subGroup);
+				if (err==B_OK) 
+					subGroup->AddData(name,type,newValue,size);
+				node->ReplaceMessage(subGroupName,subGroup);
+			}
+			else
+			{
+				if (err==B_OK) 
+					node->AddData(name,type,newValue,size);
+			}
+		}
+	}
+	undoMessage->AddMessage("selectedNodes",selectedNodes);
+	doc->SetModified();
+	settings->RemoveName("RemoveAttribute::Undo");
+	settings->AddMessage("RemoveAttribute::Undo",undoMessage);
+	settings =PCommand::Do(doc,settings);
+	return settings;
+}
+
+
+
+void RemoveAttribute::AttachedToManager(void)
+{
+}
+
+void RemoveAttribute::DetachedFromManager(void)
+{
+}
---
+++ trunk/src/Plugins/RemoveAttributeCommand/RemoveAttribute.h
@@ -0,0 +1,24 @@
+#ifndef ADD_ATTRIBUTE_H
+#define ADD_ATTRIBUTE_H
+/*
+ * @author Paradoxon powered by Jesus Christ
+ */
+#include "PCommand.h"
+#include "PDocument.h"
+
+class RemoveAttribute : public PCommand
+{
+
+public:
+							RemoveAttribute();
+	
+	//++++++++++++++++PCommand
+	virtual	BMessage*		Do(PDocument *doc, BMessage *settings);
+	virtual	void			Undo(PDocument *doc,BMessage *undo);
+	virtual	char*			Name(void){return "RemoveAttribute";};
+	virtual	void			AttachedToManager(void);
+	virtual	void			DetachedFromManager(void);
+
+	//----------------PCommand
+};
+#endif
