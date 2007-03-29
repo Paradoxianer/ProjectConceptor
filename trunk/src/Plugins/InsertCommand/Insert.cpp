5530e945127e264239fbdd98f7999edce6ac06b8
paradoxon <paradoxon@f08798fa-4112-0410-b753-e4f865caae7a>
2007-03-29 08:34:48 +0000

    Insert * change the way to insert nodes so that it should work with groups
    git-svn-id: http://svn.osdrawer.net/projectconcept@140 f08798fa-4112-0410-b753-e4f865caae7a

--- trunk/src/Plugins/InsertCommand/Insert.cpp
+++ trunk/src/Plugins/InsertCommand/Insert.cpp
@@ -36,38 +36,32 @@ BMessage* Insert::Do(PDocument *doc, BMessage *settings)
 	BMessage		*node				= NULL;
 	BList			*parentGroupList	= NULL;
 	BList			*changed			= doc->GetChangedNodes();
-	BList			*allConnectinos		= doc->GetAllConnections();
-	BList			*allNodes			= doc->GetAllNodes();
+	BList			*allConnections		= NULL;
+	BList			*allNodes			= NULL;
 	int32			i					= 0;
+	if 	((settings->FindPointer("allNodes",(void **)&allNodes)!=B_OK) || (allNodes==NULL))
+		allNodes = doc->GetAllNodes();
+	if ((settings->FindPointer("allConnections",(void **)&allConnections) != B_OK) || (allConnections == NULL))
+		allConnections = doc->GetAllConnections();
+
 	while (settings->FindPointer("node",i,(void **)&node) == B_OK)
 	{
-		if (settings->FindPointer("parentGroupList",i,(void **)&parentGroupList) == B_OK)
-			parentGroupList->AddItem(node);
+		if (node->what != P_C_CONNECTION_TYPE)
+			allNodes->AddItem(node);
 		else
+			allConnections->AddItem(node);
+		//recalc size
+		//**check if there is a passed "docRect"
+		BRect	insertFrame		= BRect(0,0,0,0);
+		if (node->FindRect("Frame",&insertFrame))
 		{
-			if (node->what != P_C_CONNECTION_TYPE)
-				allNodes->AddItem(node);
-			else
-				allConnectinos->AddItem(node);
-			//recalc size
-			BRect	insertFrame		= BRect(0,0,0,0);
-			
-			if (node->FindRect("Frame",&insertFrame))
-			{
-				BRect	docRect			= doc->Bounds();
-				if (insertFrame.bottom >= docRect.Height())
-				{
-					docRect.bottom= insertFrame.bottom+20;
-				}
-				if (insertFrame.right >= docRect.Width())
-				{
-					docRect.right = insertFrame.right+20;
-				}
-				if (docRect != doc->Bounds())
-				{
-					doc->Resize(docRect.right,docRect.bottom);
-				}
-			}
+			BRect	docRect			= doc->Bounds();
+			if (insertFrame.bottom >= docRect.Height())
+				docRect.bottom= insertFrame.bottom+20;
+			if (insertFrame.right >= docRect.Width())
+				docRect.right = insertFrame.right+20;
+			if (docRect != doc->Bounds())
+				doc->Resize(docRect.right,docRect.bottom);
 		}
 		i++;
 		if (!changed->HasItem(node))
