af08486e3b3568e96fe1da8e4d5f8be95bf28c8e
stargater <stargater@f08798fa-4112-0410-b753-e4f865caae7a>
2008-01-25 22:51:43 +0000

    Add Window Flags
    git-svn-id: http://svn.osdrawer.net/projectconcept@196 f08798fa-4112-0410-b753-e4f865caae7a

--- trunk/src/AboutWindow.cpp
+++ trunk/src/AboutWindow.cpp
@@ -2,32 +2,22 @@
 #include <interface/Rect.h>
 #include <interface/StringView.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream.h>
-
 #include "AboutWindow.h"
 #include "AboutView.h"
 #include "ProjectConceptorDefs.h"
 #include "SvnInfo.h"
 
-AboutWindow::AboutWindow():BWindow(BRect(100,100,500,350),_T("About"),B_FLOATING_WINDOW,0)
+AboutWindow::AboutWindow():BWindow(BRect(100,100,500,350),_T("About"),B_FLOATING_WINDOW,B_NOT_RESIZABLE | B_NOT_ZOOMABLE,0)
 {
 	TRACE();
 	BScreen *tmpScreen	= new BScreen();
 	BRect windowRect	= tmpScreen->Frame();
 	MoveTo((windowRect.Width()-Bounds().Width())/2,(windowRect.Height()-Bounds().Height())/2);
 	
-	#ifdef REV_NUMBER
-	AddChild(new BStringView(Bounds(),"VersionsString",REV_NUMBER));
-	#else
-	AddChild(new BStringView(Bounds(),"VersionsString",SVN_REV_STR));
-	AddChild(new BStringView(Bounds(),"VersionsString",SVN_BUILD_DATE_STR));
-	#endif
+	AddChild(new BStringView(Bounds(),"VersionsString",SVN_REV_STR " | " SVN_BUILD_DATE_STR));
 	
 	AboutView* av = new AboutView(windowRect);
 	AddChild(av);
-	//std::cout << "hallo" << std::endl;
 	
 }
 AboutWindow::~AboutWindow()
