71b153f4714d2105f0ba3beba10dcba2d49199ee
stargater <stargater@f08798fa-4112-0410-b753-e4f865caae7a>
2008-01-26 14:16:34 +0000

    Add a Precompiler for build date and make a AboutLogo.h this is not finish yet
    git-svn-id: http://svn.osdrawer.net/projectconcept@210 f08798fa-4112-0410-b753-e4f865caae7a

--- trunk/src/AboutView.cpp
+++ trunk/src/AboutView.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 
 #include <interface/Screen.h>
 #include <interface/Rect.h>
@@ -26,6 +25,11 @@ AboutView::AboutView(BRect frame):BView(frame, "", B_FOLLOW_ALL, B_WILL_DRAW)
 	fRevNumbRaw.SetTo(SVN_REV_STR);
 	kStrLength = fRevNumbRaw.Length()-7;
 	fRevNumbRaw.MoveInto(fRevNumb, 5, kStrLength);
+	//String Insert Titel and __DATE__
+	fRevNumb.Insert("revision: ",1);
+	fRevNumb.Insert("    ",fRevNumb.Length());
+	fRevNumb.Insert("build date: ",fRevNumb.Length());
+	fRevNumb.Insert(__DATE__,fRevNumb.Length());
 	
 	AddChild(new BStringView(BRect(0,kH-20,kW,kH),"VersionsString",fRevNumb.String()));
 }
