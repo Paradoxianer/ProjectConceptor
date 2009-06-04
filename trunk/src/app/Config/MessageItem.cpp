9f06f87bd7475105a7fecf8fbedac91e987e19e6
paradoxon <paradoxon@f08798fa-4112-0410-b753-e4f865caae7a>
2009-06-04 22:11:56 +0000

    removed the files wich belong to the the old concept
    git-svn-id: http://svn.osdrawer.net/projectconcept@431 f08798fa-4112-0410-b753-e4f865caae7a

--- trunk/src/app/Config/MessageItem.cpp
+++
@@ -1,8 +0,0 @@
-#include "MessageItem.h"
-#include <stdio.h>
-
-
-MessageItem::MessageItem(char *text, BMessage *message, uint32 level, bool expanded): BStringItem(text, level, expanded)
-{
-	container=message;
-}
