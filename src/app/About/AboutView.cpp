29fd9b6a64374f9c9de657f394efe2c7ffa1cd25
Paradoxon <two4god@gmail.com>
2016-06-21 21:40:46 +0200

    added the localefiles from Dario for spanish

--- src/app/About/AboutView.cpp
+++ src/app/About/AboutView.cpp
@@ -137,7 +137,7 @@ AboutView::AboutView(BRect frame):BView(frame, "", B_FOLLOW_ALL, B_WILL_DRAW)
 	AddChild(fTranslatorTitleView);
 
 	//TranslatorNameView
-	fTranslatorNameString.SetTo("- thaflo");
+	fTranslatorNameString.SetTo("- thaflo\n- Dario\n");
 	fTranslatorNameView = new BTextView(BRect(20+30+(kDL*(12/1.5)+kCL*(12/1.5)),120,kTL*(12/1.5)+(kDL*(12/1.5)+kCL*(12/1.5)-9+40),frameHeight-70), "translatornameview",
 	BRect(0,0,frameWidth-15,15), B_FOLLOW_ALL_SIDES, B_WILL_DRAW | B_FULL_UPDATE_ON_RESIZE);
 	fTranslatorNameView->MakeSelectable(false);
